4218e6e4d542519352e7042857772cff24c44f19
hasanmainul <mainul.haque@framgia.com>
2016-09-09 20:51:42 +0600

    SimpleAnimation101

---
+++ SimpleAnimation101/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  SimpleAnimation101
+//
+//  Created by Md Mainul Haque on 9/9/16.
+//  Copyright © 2016 Md Mainul Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var slidingLabel: UILabel!
+    @IBOutlet weak var birdTypeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func slideTextButtonTapped(sender: AnyObject) {
+        self.slidingLabel.slideInFromLeft()
+        self.slidingLabel.text = "Sliding Text!"
+        self.slidingLabel.textColor = UIColor.blueColor()
+    }
+    
+    @IBAction func updateBirdTypeLabel(sender: UIButton) {
+        self.birdTypeLabel.fadeOut(completion: {
+            (finished: Bool) -> Void in
+            self.birdTypeLabel.text = "Fly Away"
+            self.birdTypeLabel.fadeIn()
+        })
+    }
+
+}
+
+extension UIView {
+    // Name this function in a way that makes sense to you...
+    // slideFromLeft, slideRight, slideLeftToRight, etc. are great alternative names
+    func slideInFromLeft(duration: NSTimeInterval = 1.0, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition animation
+        let slideInFromLeftTransition = CATransition()
+        
+        // Set its callback delegate to the completionDelegate that was provided (if any)
+        if let delegate: AnyObject = completionDelegate {
+            slideInFromLeftTransition.delegate = delegate
+        }
+        
+        // Customize the animation's properties
+        slideInFromLeftTransition.type = kCATransitionPush
+        slideInFromLeftTransition.subtype = kCATransitionFromLeft
+        slideInFromLeftTransition.duration = duration
+        slideInFromLeftTransition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        slideInFromLeftTransition.fillMode = kCAFillModeRemoved
+        
+        // Add the animation to the View's layer
+        self.layer.addAnimation(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
+    }
+    
+    func fadeIn(duration: NSTimeInterval = 1.0, delay: NSTimeInterval = 0.0, completion: ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animateWithDuration(duration, delay: delay, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.alpha = 1.0
+            }, completion: completion)  }
+    
+    func fadeOut(duration: NSTimeInterval = 1.0, delay: NSTimeInterval = 0.0, completion: (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animateWithDuration(duration, delay: delay, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.alpha = 0.0
+            }, completion: completion)
+    }
+}
